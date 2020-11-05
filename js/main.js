2b55e5a2a9be3e5cc06e4b751dc4276d1b8270e7
Oksana <oksana.shpack@gmail.com>
2020-11-05 20:35:31 +1100

    responsive

--- js/main.js
+++ js/main.js
@@ -3,10 +3,16 @@ $(function () {
     $('.slider-blog__inner').slick({
         dots: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt=""></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt=""></button>'
+        nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt=""></button>',
+        responsive: [{
+            breakpoint: 769,
+            settings: {
+                arrows: false
+            }
+        }, ]
     });
 
-    $('.menu__btn').on('click',function(){
+    $('.menu__btn').on('click', function () {
         $('.header__top-inner').toggleClass('header__top-inner--active');
     });
 
